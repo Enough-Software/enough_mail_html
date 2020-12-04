a0a8f865c589d19c34ee2313d63934a634a07a02
Robert Virkus <robert.virkus@enough.de>
2020-12-04 21:17:28 +0100

    fix transformation of plain text messages

--- lib/src/text/text_transformers.dart
+++ lib/src/text/text_transformers.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:enough_mail/mime_message.dart';
 import 'package:enough_mail_html/enough_mail_html.dart';
 
@@ -22,7 +24,17 @@ class MergeAttachedImageTextProcessor extends TextTransformer {
     while ((nextImageDefinition = search.next()) != null) {
       final cid = nextImageDefinition.substring(
           '[cid:'.length, nextImageDefinition.length - 2);
+      final part = message.getPartWithContentId(cid);
+      if (part != null) {
+        final contentType = part.getHeaderContentType();
+        final mediaType = contentType?.mediaType?.text ?? 'image/png';
+        final binary = part.decodeContentBinary();
+        final base64Data = base64Encode(binary);
+        text.replaceFirst(nextImageDefinition,
+            '<img src="data:$mediaType;base64,$base64Data" alt="${part.getHeaderContentDisposition()?.filename}"/>');
+      }
     }
+    return text;
   }
 }
 
