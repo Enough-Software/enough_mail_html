9231370ed29a78094a53fad1bc288bba890a4143
Robert Virkus <robert.virkus@enough.de>
2021-01-04 14:10:00 +0100

    ensuring utf-8 encoding fix #4

--- lib/src/dom/meta_transformers.dart
+++ lib/src/dom/meta_transformers.dart
@@ -5,6 +5,8 @@ import 'package:html/dom.dart';
 class ViewPortTransformer implements DomTransformer {
   static final Element _viewPortMetaElement = Element.html(
       '<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+  static final Element _contentTypeMetaElement = Element.html(
+      '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">');
 
   const ViewPortTransformer();
 
@@ -13,12 +15,22 @@ class ViewPortTransformer implements DomTransformer {
       TransformConfiguration configuration) {
     final metaElements = document.getElementsByTagName('meta');
     var viewportNeedsToBeAdded = true;
+    var contentTypeNeedsToBeAdded = true;
     for (final metaElement in metaElements) {
       if (metaElement.attributes['name'] == 'viewport') {
         viewportNeedsToBeAdded = false;
         metaElement.attributes['content'] =
             'width=device-width, initial-scale=1.0';
+      } else {
+        final httpEquiv = metaElement.attributes['http-equiv'];
+        if (httpEquiv != null && httpEquiv.toLowerCase() == 'content-type') {
+          contentTypeNeedsToBeAdded = false;
+          metaElement.attributes['content'] = 'text/html; charset=utf-8';
+        }
       }
+    }
+    if (contentTypeNeedsToBeAdded) {
+      document.head.append(_contentTypeMetaElement);
     }
     if (viewportNeedsToBeAdded) {
       document.head.append(_viewPortMetaElement);
